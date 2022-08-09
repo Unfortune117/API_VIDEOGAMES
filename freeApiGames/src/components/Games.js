8a84b5149a4737be1dbfbc6d75471a87aa15c7ef
Rocolas2000 <91926184+Rocolas2000@users.noreply.github.com>
2022-08-09 11:59:19 -0500

    estilos css

--- freeApiGames/src/components/Games.js
+++ freeApiGames/src/components/Games.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import "./GamesStyle.css"
 
 export const Games = ({games=[]}) => {  //[] this is array, if you don't receive anything, it's empty
   
   
   
   return (
-    <div className="row">
-        {games.map((item,index)=>(
-            <div key={index} className="col">
-                <div className="card">
-                    <img src={item.thumbnail} alt=""></img>
-                    <div className="card-body">
-                      <h5 className="card-title">{item.title}</h5>
-                      <hr></hr>
-                      <p></p>
-                    </div>
-                </div>
-            </div>
-        ))}
+    <div className="container-fluid" id="contfluid">
+      <div className="row">
+          {games.map((item,index)=>(
+              <div key={index} className="col-lg-4 col-md-4 col-sm-6 col-12">
+                  <div className="card">
+                      <img src={item.thumbnail} alt=""></img>
+                      <div className="card-body">
+                        <h5 className="card-title">{item.title}</h5>
+                        <hr></hr>
+                        <p className="card-text">{item.platform}</p>
+                      </div>
+                  </div>
+              </div>
+          ))}
+      </div>
     </div>
   )
 }
--- freeApiGames/src/components/Pagination.js
+++ freeApiGames/src/components/Pagination.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./PaginationStyle.css"
 
 export const Pagination = (props) => {
     
   return (
     <nav>
-        <ul className="pagination justify-content-center">
+        <ul className="pagination justify-content-center" id="pagcont">
             <li className="page-item">
                 <button className="page-link" onClick={props.prevHandler} >Previous</button>
             </li>
